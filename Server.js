67e19044ae4590d02a80ca269831ae675312e4af
DarknessInsideU <51133202+Abdallah2358@users.noreply.github.com>
2023-01-20 18:58:01 +0200

    addedd some routes

--- Server.js
+++ Server.js
@@ -3,18 +3,29 @@ const fs = require('fs');
 const app = express()
 app.use(require('body-parser').urlencoded({ extended: false }));
 const port = 3000
-var daata ;
+var Reservation = [] ;
+var Rooms = [];
 fs.readFile('Data/Reservation.json', function (err, data) {
-    daata = JSON.parse(data);
+    Reservation = JSON.parse(data);
     return;
 });
-app.get('/', (req, res) => {
-    res.send(req.body)
-})
-app.post('/aa', (req, res) => {
-
+fs.readFile('Data/Rooms.json', function (err, data) {
+    Rooms = JSON.parse(data);
+    return;
+});
+app.get('/rooms', (req, res) => {
     
-    res.send(req.body)
+    res.send(Rooms)
+})
+app.post('/rooms', (req, res) => {
+    res.send(req.body['start'])
+    Rooms.push(req.body);
+    fs.writeFile('Data/Rooms.json' ,Rooms );
+})
+app.post('/reserv', (req, res) => {
+    res.send(req.body['start'])
+    Rooms.push(req.body);
+    fs.writeFile('Data/Rooms.json' ,Rooms );
 })
 app.get('/send', (req, res) => {
     res.send(req)
